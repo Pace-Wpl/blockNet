cfbcbed5f6b1edeeb777932d514d795ccf9f0c11
Pace-Wpl <910260819@qq.com>
2020-04-20 13:26:38 +0800

    client

--- client/main.go
+++ client/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/blockNet/client/service"
 	"github.com/julienschmidt/httprouter"
 )
+
+type middleWare struct {
+	r *httprouter.Router
+}
+
+func NewMiddleWare(r *httprouter.Router) http.Handler {
+	m := middleWare{}
+	m.r = r
+	return m
+}
+
+func (m middleWare) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("request ip:%s\n", r.RemoteAddr)
+	if strings.Split(r.RemoteAddr, ":")[0] != "127.0.0.1" {
+		w.Write([]byte("bad request."))
+		return
+	} else {
+		w.Write([]byte("auth pass!"))
+	}
+	m.r.ServeHTTP(w, r)
+}
 
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
@@ -19,6 +42,7 @@ func RegisterHandlers() *httprouter.Router {
 func main() {
 	defer service.ServiceClient.Sdk.Close()
 	r := RegisterHandlers()
-	http.ListenAndServe(":8000", r)
+	m := NewMiddleWare(r)
+	http.ListenAndServe(":8000", m)
 
 }
